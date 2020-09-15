df4d06b9f3533a8e02f45a5ad5fd76601e25269a
franklili <348104201@qq.com>
2020-09-15 18:08:59 +0800

    replace app with application

--- application.py
+++ application.py
@@ -17,17 +17,17 @@ from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(application, db)
 
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Follow=Follow, Role=Role,
                 Permission=Permission, Post=Post, Comment=Comment)
 
 
-@app.cli.command()
+@application.cli.command()
 @click.option('--coverage/--no-coverage', default=False,
               help='Run tests under code coverage.')
 @click.argument('test_names', nargs=-1)
@@ -56,7 +56,7 @@ def test(coverage, test_names):
         COV.erase()
 
 
-@app.cli.command()
+@application.cli.command()
 @click.option('--length', default=25,
               help='Number of functions to include in the profiler report.')
 @click.option('--profile-dir', default=None,
@@ -64,12 +64,12 @@ def test(coverage, test_names):
 def profile(length, profile_dir):
     """Start the application under the code profiler."""
     from werkzeug.contrib.profiler import ProfilerMiddleware
-    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+    application.wsgi_app = ProfilerMiddleware(application.wsgi_app, restrictions=[length],
                                       profile_dir=profile_dir)
-    app.run()
+    application.run()
 
 
-@app.cli.command()
+@application.cli.command()
 def deploy():
     """Run deployment tasks."""
     # migrate database to latest revision
